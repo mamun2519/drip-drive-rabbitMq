2ab43ac6b28d391670920b607434940e774ec94e
mamun2519 <juborajislam46@gmail.com>
2025-06-02 20:17:06 +0600

    feat: added to consumer

---
+++ consumer.js
@@ -0,0 +1,34 @@
+import amqplib from "amqplib";
+
+const sendMessage = async () => {
+  const connection = await amqplib.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  const queue = "message_queue";
+  const exchange = "message_exchange";
+  const routingKey = "message_routing_key";
+
+  // create a exchange
+  await channel.assertExchange(exchange, "direct", { durable: false });
+  // create a queue
+  await channel.assertQueue(queue, { durable: false });
+  // bind the queue to the exchange
+  await channel.bindQueue(queue, exchange, routingKey);
+
+  //message to send
+  const message = {
+    text: "Hello, this is a message from the producer!",
+    timestamp: new Date().toISOString(),
+  };
+  // send the message
+  channel.publish(exchange, routingKey, Buffer.from(JSON.stringify(message)));
+
+  console.log("Message sent");
+
+  setTimeout(() => {
+    channel.close();
+    connection.close();
+  }, 500);
+};
+
+sendMessage();
