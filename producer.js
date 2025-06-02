956ddcb07860ba36b953a1a85a75249462b7d632
mamun2519 <juborajislam46@gmail.com>
2025-06-02 20:05:30 +0600

    feat: added to producer

--- producer.js
+++ producer.js
@@ -1,3 +1,6 @@
 import amqplib from "amqplib";
 
-const sendMessage = async () => {};
+const sendMessage = async () => {
+  const connection = await amqplib.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+};
