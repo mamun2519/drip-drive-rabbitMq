b68cf7ec031c6aef4a5633b2521667c8da483bca
mamun2519 <juborajislam46@gmail.com>
2025-06-03 11:44:19 +0600

    feat: added to multiple queue busniess logic

--- multiple_queue/producer.js
+++ multiple_queue/producer.js
@@ -4,8 +4,11 @@ const producer = async () => {
   const connection = await amqplib.connect("amqp://localhost");
   const channel = await connection.createChannel();
 
+  // queue
   const user_send_mail_queue = "user_send_mail_queue";
   const customer_send_mail_queue = "customer_send_mail_queue";
+  // routing key
+  const routing_key_for_user = "user_routing_key";
 };
 
 producer();
