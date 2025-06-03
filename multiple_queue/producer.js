5022319bcfaca5a15d12ad5cdfb7a09741ebe53d
mamun2519 <juborajislam46@gmail.com>
2025-06-03 11:56:27 +0600

    feat: added to multiple queue busniess logic

--- multiple_queue/producer.js
+++ multiple_queue/producer.js
@@ -21,6 +21,13 @@ const producer = async () => {
     bodyL: "Hello world",
   };
 
+  const CustomerMessage = {
+    to: "mamun@gmail.com",
+    form: "programmingHero@gmail.com",
+    subject: "Thank You",
+    bodyL: "Hello world",
+  };
+
   // create exchange
   await channel.assertExchange(exchange, "direct", { durable: false });
   // create queue
@@ -32,15 +39,28 @@ const producer = async () => {
   await channel.bindQueue(
     customer_send_mail_queue,
     exchange,
-    routing_key_for_user
+    routing_key_for_customer
   );
 
   // publish message
   channel.publish(
     exchange,
-    user_send_mail_queue,
-    Buffer.from(JSON.stringify(message))
+    routing_key_for_user,
+    Buffer.from(JSON.stringify(UserMessage))
   );
+
+  channel.publish(
+    exchange,
+    routing_key_for_customer,
+    Buffer.from(JSON.stringify(UserMessage))
+  );
+
+  console.log("Message sent");
+
+  setTimeout(() => {
+    channel.close();
+    connection.close();
+  }, 500);
 };
 
 producer();
