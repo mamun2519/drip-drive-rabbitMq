d8e4f92c1868df001600fb3b8f69df3a43dfa421
mamun2519 <juborajislam46@gmail.com>
2025-06-03 12:03:58 +0600

    feat: added to customer  consumer busniess logic

--- multiple_queue/customer_consumer.js
+++ multiple_queue/customer_consumer.js
@@ -1,5 +1,11 @@
 import amqplib from "amqplib";
 
-const customerConsumer = async () => {};
+const customerConsumer = async () => {
+  const connection = await amqplib.connect("amqp://localhost");
+  const channel = await connection.createChannel();
+
+  //queue
+  const customer_send_mail_queue = "customer_send_mail_queue";
+};
 
 customerConsumer();
