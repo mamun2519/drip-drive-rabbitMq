d902b85582334c55d1f4b1742f62ef7cb44a356c
mamun2519 <juborajislam46@gmail.com>
2025-06-05 14:45:20 +0600

    added to topic concept

--- topic/orderNotifaction.services.js
+++ topic/orderNotifaction.services.js
@@ -4,7 +4,7 @@ const OrderNotificationServices = async () => {
   const connection = await amqplib.connect("amqp://localhost");
   const channel = await connection.createChannel();
   const exchange = "notification_exchange";
-  const queue = "order_queue"
-const exchangeType = "topic";
-  await 
+  const queue = "order_queue";
+  const exchangeType = "topic";
+  await channel.assertExchange(exchange, exchangeType, { durable: true });
 };
