a2e2db646fb39d7a4757ecd3caa3da7c32a43dfe
mamun2519 <juborajislam46@gmail.com>
2025-06-05 14:51:25 +0600

    added to topic concept

--- topic/paymentNotification.services.js
+++ topic/paymentNotification.services.js
@@ -3,6 +3,10 @@ import amqplib from "amqplib";
 const paymentNotificationServices = async () => {
   const connection = await amqplib.connect("amqp://localhost");
   const channel = await connection.createChannel();
+
+  const exchange = "notification_exchange";
+  const exchangeType = "topic";
+  const queue = "payment_queue";
 };
 
 paymentNotificationServices();
