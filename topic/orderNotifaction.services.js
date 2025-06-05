63bc01bad30440fd05e731849cce590bf2cbe471
mamun2519 <juborajislam46@gmail.com>
2025-06-05 14:41:18 +0600

    added to topic concept

--- topic/orderNotifaction.services.js
+++ topic/orderNotifaction.services.js
@@ -1,1 +1,6 @@
 import amqplib from "amqplib";
+
+const OrderNotificationServices = () => {
+        const connection = await amqplib.connect("amqp://localhost");
+        const channel = await connection.createChannel();
+};
